451c629d249f7a7a7203f91fc8b04accef4a770d
abhiparmar101 <abhi@ambiplatforms.com>
2022-12-15 12:57:46 +0530

    add feat: Person counter

---
+++ face_detection_model/FR/facial_recognition_model_training.py
@@ -0,0 +1,65 @@
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import KFold
+# from keras.models import load_model
+import matplotlib.pyplot as plt
+# from softmax import SoftMax
+import numpy as np
+import argparse
+import pickle
+
+from FR.softmax import SoftMax
+
+import os
+
+class TrainFaceRecogModel:
+
+    def __init__(self):
+        # Load the face embeddings
+        self.data = pickle.loads(open(os.path.sep.join(
+            [str(os.getcwd()) ,"FR/faceEmbeddingModels/embeddings.pickle"]), "rb").read())
+
+    def trainKerasModelForFaceRecognition(self):
+        # Encode the labels
+        le = LabelEncoder()
+        labels = le.fit_transform(self.data["names"])
+        num_classes = len(np.unique(labels))
+        labels = labels.reshape(-1, 1)
+        one_hot_encoder = OneHotEncoder()
+        labels = one_hot_encoder.fit_transform(labels).toarray()
+
+        embeddings = np.array(self.data["embeddings"])
+
+        # Initialize Softmax training model arguments
+        BATCH_SIZE = 8
+        EPOCHS = 5
+        input_shape = embeddings.shape[1]
+
+        # Build sofmax classifier
+        softmax = SoftMax(input_shape=(input_shape,), num_classes=num_classes)
+        model = softmax.build()
+
+        # Create KFold
+        cv = KFold(n_splits = 5, random_state = 42, shuffle=True)
+        history = {'acc': [], 'val_acc': [], 'loss': [], 'val_loss': []}
+
+        # Train
+        for train_idx, valid_idx in cv.split(embeddings):
+            X_train, X_val, y_train, y_val = embeddings[train_idx], embeddings[valid_idx], labels[train_idx], labels[valid_idx]
+            his = model.fit(X_train, y_train, batch_size=BATCH_SIZE, epochs=EPOCHS, verbose=1, validation_data=(X_val, y_val))
+
+
+            history['acc'] += his.history['accuracy']
+            history['val_acc'] += his.history['val_accuracy']
+            history['loss'] += his.history['loss']
+            history['val_loss'] += his.history['val_loss']
+
+            print(f"[INFO] Accuracy = {history['acc']}")
+
+        # write the face recognition model to output
+        model.save(os.path.sep.join(
+            [str(os.getcwd()) ,"FR/faceEmbeddingModels/my_model.h5"]))
+        f = open(os.path.sep.join(
+            [str(os.getcwd()), "FR/faceEmbeddingModels/le.pickle"]), "wb")
+        f.write(pickle.dumps(le))
+        f.close()
---
+++ face_detection_model/FR/generate_face_embeddings.py
@@ -0,0 +1,63 @@
+
+from  mainproject.FR import face_model
+from imutils import paths
+import numpy as np
+import pickle
+import cv2
+import os
+
+
+
+class GenerateFaceEmbedding:
+
+    def __init__(self):
+        self.image_size = '112,112'
+        self.model = os.path.sep.join(
+            [str(os.getcwd()), "FR/models/model-y1-test2/model,0"])
+        self.embedding_model_path = os.path.sep.join(
+            [str(os.getcwd()), "FR/faceEmbeddingModels/embeddings.pickle"])
+        self.threshold = 1.24
+        self.det = 0
+
+    def genFaceEmbedding(self, path):
+        # Grab the paths to the input images in our dataset
+        print("[INFO] quantifying faces...")
+        imagePaths = list(paths.list_images(path))
+
+        # Initialize the faces embedder
+        embedding_model = face_model.FaceModel(self.image_size, self.model, self.threshold, self.det)
+
+        # Initialize our lists of extracted facial embeddings and corresponding people names
+        knownEmbeddings = []
+        knownNames = []
+
+        # Initialize the total number of faces processed
+        total = 0
+
+        # Loop over the imagePaths
+        for (i, imagePath) in enumerate(imagePaths):
+            # extract the person name from the image path
+            print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
+            name = imagePath.split(os.path.sep)[-2]
+
+            # load the image
+            image = cv2.imread(imagePath)
+            # convert face to RGB color
+            nimg = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            nimg = np.transpose(nimg, (2, 0, 1))
+            # Get the face embedding vector
+            face_embedding = embedding_model.get_feature(nimg)
+
+            # add the name of the person + corresponding face
+            # embedding to their respective list
+            knownNames.append(name)
+            knownEmbeddings.append(face_embedding)
+            total += 1
+
+        print(total, " faces embedded")
+
+        # save to output
+        data = {"embeddings": knownEmbeddings, "names": knownNames}
+        f = open(self.embedding_model_path, "wb")
+        f.write(pickle.dumps(data))
+        f.close()

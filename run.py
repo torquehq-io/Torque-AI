56fdee9f066f56ab0fb509404ae37eb85d7dcda3
Abhiparmar101 <abhi@ambiplatforms.com>
2023-03-14 11:28:54 +0530

    solve sidebar issue in multi detec object

--- run.py
+++ run.py
@@ -1717,132 +1717,8 @@ def output():
     return Response(out(), mimetype='text/html')
 
 ########################################################################3
-######################### multi streaming ###################################
-# import sys
-
-# arg1 = sys.argv[1]
-# arg2 = sys.argv[2]
-
-# testing = arg1
-# print(testing)
-
-# import torch
-# import numpy as np
-# import cv2
-# import time
 
 
-# class  ObjectDetection :
-#     """
-#     Class implements Yolo5 model to make inferences on a youtube video using OpenCV.
-#     """
-
-#     def __init__(self):
-#         """
-#         Initializes the class with youtube url and output file.
-#         :param url: Has to be as youtube URL,on which prediction is made.
-#         :param out_file: A valid output file name.
-#         """
-#         self.model = self.load_model()
-#         self.classes = self.model.names
-#         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
-#         print("\n\nDevice Used:",self.device)
-#         current_loggin_user=current_user.username
-#         self.fetch_url =  User_camera_sources.query.filter_by(username=current_loggin_user).first()
-#         print(self.fetch_url.link1)
-
-
-
-#     def load_model(self):
-#         """
-#         Loads Yolo5 model from pytorch hub.
-#         :return: Trained Pytorch model.
-#         """
-#         model = torch.hub.load("ultralytics/yolov5", "custom", path = "./yolov5s.pt",device='cpu', force_reload=True)
-#         return model
-
-
-#     def score_frame(self, frame):
-#         """
-#         Takes a single frame as input, and scores the frame using yolo5 model.
-#         :param frame: input frame in numpy/list/tuple format.
-#         :return: Labels and Coordinates of objects detected by model in the frame.
-#         """
-#         self.model.to(self.device)
-#         frame = [frame]
-#         results = self.model(frame)
-
-#         labels, cord = results.xyxyn[0][:, -1], results.xyxyn[0][:, :-1]
-#         return labels, cord
-
-
-#     def class_to_label(self, x):
-#         """
-#         For a given label value, return corresponding string label.
-#         :param x: numeric label
-#         :return: corresponding string label
-#         """
-#         return self.classes[int(x)]
-
-
-#     def plot_boxes(self, results, frame):
-#         """
-#         Takes a frame and its results as input, and plots the bounding boxes and label on to the frame.
-#         :param results: contains labels and coordinates predicted by model on the given frame.
-#         :param frame: Frame which has been scored.
-#         :return: Frame with bounding boxes and labels ploted on it.
-#         """
-#         labels, cord = results
-#         n = len(labels)
-#         x_shape, y_shape = frame.shape[1], frame.shape[0]
-#         for  i  in  range ( n ):
-#             row = cord[i]
-#             if row[4] >= 0.2:
-#                 x1, y1, x2, y2 = int(row[0]*x_shape), int(row[1]*y_shape), int(row[2]*x_shape), int(row[3]*y_shape)
-#                 bgr = (0, 255, 0)
-#                 cv2.rectangle(frame, (x1, y1), (x2, y2), bgr, 2)
-#                 cv2.putText(frame, self.class_to_label(labels[i]), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.9, bgr, 2)
-
-#         return frame
-
-
-#     def __call__(self):
-#         """
-#         This function is called when class is executed, it runs the loop to read the video frame by frame,
-#         and write the output into a new file.
-#         :return: void
-#         """
-#         import subprocess as sp
-        
-#         cap = cv2.VideoCapture("rtmp://media5.ambicam.com:1938/live/1efa24f9-0cd0-47c5-b604-c7e3ee118302")
-#         frame_width = int(cap.get(3))
-#         frame_height = int(cap.get(4))
-#         rtmp_url = "rtmp://media5.ambicam.com:1938/live/test12"
-#         #ffmpeg = "ffmpeg -stream_loop -1 -f rawvideo -pix_fmt bgr24 -s {}x{} -r 10 -i - -c:v libx264 -f flv {}".format(frame_width, frame_height, rtmp_url)
-#         ffmpeg = "ffmpeg -f rawvideo -pix_fmt bgr24 -s {}x{} -r 15 -i - -pix_fmt yuv420p -vcodec libx264 -ar 8K -f flv {}".format(
-#             frame_width, frame_height, rtmp_url)
-
-#         process = sp.Popen(ffmpeg.split(), stdin=sp.PIPE)
-#         while cap.isOpened():
-
-#             start_time = time.perf_counter()
-#             ret, frame = cap.read()
-#             if not ret:
-#                 break
-#             results = self.score_frame(frame)
-#             frame = self.plot_boxes(results, frame)
-#             end_time = time.perf_counter()
-#             fps = 1 / np.round(end_time - start_time, 3)
-#             cv2.putText(frame, f'FPS: {int(fps)}', (20,70), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (0,255,0), 2)
-            
-
-# @app.route('/output_4multi')
-# def output_4multi():
-
-#     # Create a new object and execute.
-#     detection = ObjectDetection()
-#     #detection()
-#     return Response( detection())
 
 #########################################################################
 ########## multi object detection list ###################3
@@ -1963,8 +1839,19 @@ import time
 import torch
 import subprocess as sp
 import ffmpegfeed.feeds as fct 
+
+@app.route('/multi_model_excution')
+def multi_model_excution():
+ 
+    
+    return render_template('home/detection.html',User_Models_record=User_Models_record.query.filter_by(username=current_user.username))
+
+
+
+
 class multifeed1():
     def __init__(self, url):
+    
         # self.video = cv2.VideoCapture('rtmp://media1.ambicam.com:1938/dvr7/fd9b67cc-6c2e-46c6-99c4-0e13ac403e32')
         current_loggin_user=current_user.username
         fetch_url =  User_camera_sources.query.filter_by(username=current_loggin_user).first()
@@ -2028,6 +1915,8 @@ def gen_multifeed1(camera):
 
 @app.route('/video_feed_multifeed1')
 def video_feed_multifeed1():
+    selectValue = request.form.get('model_name')
+    print(selectValue)
     url = request.args.get('url')
     
     return Response(gen_multifeed1(multifeed1(url)), mimetype='multipart/x-mixed-replace; boundary=frame')
